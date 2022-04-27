0f278215f7fbb8ad0e709b2c604db3ba7982ba2c
AmandaPires <102668847+AmandaPires17@users.noreply.github.com>
2022-04-27 08:50:16 -0300

    Add files via upload

---
+++ amanda_25-04/src/components/BlocoUm.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <article class="block">
+    <h1>Amanda</h1>
+  </article>
+
+</template>
---
+++ amanda_25-04/src/components/MainPrincipal.vue
@@ -0,0 +1,26 @@
+<script>
+import BlocoUm from "@/components/BlocoUm.vue";
+
+export default {
+  components: { BlocoUm },
+};
+</script>
+<template>
+  <main id="content">
+    <BlocoUm id="bloco1" />
+
+    <BlocoUm id="bloco2" />
+
+    <BlocoUm id="bloco3" />
+
+    <BlocoUm id="bloco4" />
+
+    <BlocoUm id="bloco5" />
+
+    <BlocoUm id="bloco6" />
+
+    <BlocoUm id="bloco7"/>
+
+    <BlocoUm id="bloco8"/>
+  </main>
+</template>
